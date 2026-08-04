3881fe34e275c41e5adc679de566559196bff3c5
Rolando <rgarro@gmail.com>
2026-08-04 13:29:45 -0600

    initial commit

---
+++ app/src/main/java/com/emptyart/tiviveskitesurf/FindBeachActivity.java
@@ -0,0 +1,13 @@
+package com.emptyart.tiviveskitesurf;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FindBeachActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_beach);
+    }
+}
