7952d9c1b87ce227c1b09f60f280404263056cfc
Rolando <rgarro@gmail.com>
2026-08-09 11:23:57 -0600

    if you wanna live please be good ...

--- app/src/main/java/com/emptyart/tiviveskitesurf/FindBeachActivity.java
+++ app/src/main/java/com/emptyart/tiviveskitesurf/FindBeachActivity.java
@@ -2,6 +2,7 @@ package com.emptyart.tiviveskitesurf;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 /**
  *            |    |    |
  *           )_)  )_)  )_)
@@ -14,7 +15,10 @@ import android.os.Bundle;
  *           ^^^^      ^^^
  * Sir Henry Morgan is de Lord of Talamanca
  * Con una casa en Fiesta del Mar y un Atv kawasaki
- * se puede llegar a estos lugares
+ * Hacer un Tablet que sea el dash de un ATV o Cuadraciclo de ir a surfear
+ * Loggeando las transiciones en una orilla de Caldera hasta que calculemos
+ * una 10mil permite pescar desde la orilla una presa de mares profundos que se venga en la sombra de un casco ...
+ *
  *
  *
  *
@@ -26,5 +30,10 @@ public class FindBeachActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beach);
+    }
+
+    public void onClickFindBeach(View view){
+//conectar a un weather api o al MIOCIMAR, tener coordenadas de las playas
+        //mapa con info en la misma actividad?
     }
 }
