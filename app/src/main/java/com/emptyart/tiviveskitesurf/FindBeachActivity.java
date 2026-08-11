d05f59f1dcd7cc2f4dd5680fa4de3f1083f21567
Rolando <rgarro@gmail.com>
2026-08-11 14:10:35 -0600

    galong galong galong ...

--- app/src/main/java/com/emptyart/tiviveskitesurf/FindBeachActivity.java
+++ app/src/main/java/com/emptyart/tiviveskitesurf/FindBeachActivity.java
@@ -3,6 +3,9 @@ package com.emptyart.tiviveskitesurf;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.util.Log;
 /**
  *            |    |    |
  *           )_)  )_)  )_)
@@ -14,11 +17,6 @@ import android.view.View;
  *     ^^^^      ^^^^     ^^^    ^^
  *           ^^^^      ^^^
  * Sir Henry Morgan is de Lord of Talamanca
- * Con una casa en Fiesta del Mar y un Atv kawasaki
- * Hacer un Tablet que sea el dash de un ATV o Cuadraciclo de ir a surfear
- * Loggeando las transiciones en una orilla de Caldera hasta que calculemos
- * una 10mil permite pescar desde la orilla una presa de mares profundos que se venga en la sombra de un casco ...
- *
  *
  *
  *
@@ -33,7 +31,9 @@ public class FindBeachActivity extends Activity {
     }
 
     public void onClickFindBeach(View view){
-//conectar a un weather api o al MIOCIMAR, tener coordenadas de las playas
+        //conectar a un weather api o al MIOCIMAR, tener coordenadas de las playas
         //mapa con info en la misma actividad?
+        Spinner beaches = (Spinner) findViewById(R.id.beach);
+        Log.i("Beach:",beaches.toString());
     }
 }
